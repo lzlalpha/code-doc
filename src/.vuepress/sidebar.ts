d117c8740c36cdbebd84b7c05ec6eb4de8753d97
lzlalpha <lzlalpha@qq.com>
2024-02-25 11:09:05 +0800

    添加内容

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,17 +3,17 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    // {
-    //   text: "案例",
-    //   icon: "laptop-code",
-    //   prefix: "demo/",
-    //   link: "demo/",
-    //   children: "structure",
-    // },
     {
       text: "编程知识库",
       icon: "book",
       prefix: "cpp/",
+      children: "structure",
+    },
+    {
+      text: "演示案例",
+      icon: "laptop-code",
+      prefix: "demo/",
+      link: "demo/",
       children: "structure",
     },
     // {
--- src/cpp/1jichuyufa/hello-world.md
+++ src/cpp/1jichuyufa/hello-world.md
@@ -4,4 +4,21 @@ order: 20
 icon: circle-info
 ---
 
-第一篇代码...
+## 第一份代码
+
+通过这样一个示例程序来展开 C++ 入门之旅吧～
+
+注：请在编写前注意开启英文输入法。
+
+C++ 语言
+
+```c++
+#include <iostream>  // 引用头文件
+
+using namespace std; // 引入命名空间
+
+int main() {                // 定义 main 函数
+  cout << "Hello, world!";  // 输出 Hello, world!
+  return 0;                 // 返回 0，结束 main 函数
+}
+```
