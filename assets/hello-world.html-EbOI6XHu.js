485f75aeb6eec88dddb56375d2e09f76d8ff10d9
lzlalpha <lzlalpha@gmail.com>
2024-02-25 03:10:01 +0000

    Deploying to gh-pages from @ lzlalpha/code-doc@d117c8740c36cdbebd84b7c05ec6eb4de8753d97 🚀

---
+++ assets/hello-world.html-EbOI6XHu.js
@@ -0,0 +1,9 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as i,e as l}from"./app-MT2G9oNk.js";const a={},s=l(`<h2 id="第一份代码" tabindex="-1"><a class="header-anchor" href="#第一份代码"><span>第一份代码</span></a></h2><p>通过这样一个示例程序来展开 C++ 入门之旅吧～</p><p>注：请在编写前注意开启英文输入法。</p><p>C++ 语言</p><div class="language-c++ line-numbers-mode" data-ext="c++" data-title="c++"><pre class="language-c++"><code>#include &lt;iostream&gt;  // 引用头文件
+
+using namespace std; // 引入命名空间
+
+int main() {                // 定义 main 函数
+  cout &lt;&lt; &quot;Hello, world!&quot;;  // 输出 Hello, world!
+  return 0;                 // 返回 0，结束 main 函数
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),d=[s];function c(t,r){return n(),i("div",null,d)}const u=e(a,[["render",c],["__file","hello-world.html.vue"]]);export{u as default};
